1ab530b1ecb8eca945c2bd6f9251a7d0f4da004f
Pawan Channe <pawanchanne2020@gmail.com>
2022-07-03 08:51:39 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+print('Calculator')
+num=int(input('enter first number '))
+num2=int(input('enter second number '))
+ope=input('enter operator ')
+if ope=='+':
+    print(num+num2)
+    print('Addition')
+elif ope=='-':
+    print(num-num2)
+    print('Subtraction')
+elif ope=='*':
+    print(num*num2)
+    print('Multiplication')
+elif ope=='/':
+    print(num/num2)
+    print('Division')
+elif ope=='//':
+    print(num//num2)
+    print('Floor division')
+elif ope=='%':
+    print(num%num2)
+    print('Modulus')
+else:
+    print('enter a valid operator')
