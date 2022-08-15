9b23ac98e0c4b99bac4c5348e98ef0d5183097a7
Pawan Channe <pawanchanne2020@gmail.com>
2022-08-15 09:28:15 +0530

    Update and rename KBC1.py to KBC.py

--- KBC.py
+++ KBC.py
@@ -1,8 +1,6 @@
 
 
 
-
-
 question_li=["How many continents are there?","What is the capital of India?","NG mei kaun se course padhaya jaata hai?"]
 option_li=[["1 Seven", "2 Nine", "3 Four", "4 Eight"],["1 Chandigarh", "2 Bhopal", "3 Chennai", "4 Delhi"],["1 Software Engineering", "2 Counseling", "3 Tourism", "4 Agriculture"]]
 for number in range(len(question_li)):
@@ -41,15 +39,6 @@ for number in range(len(question_li)):
         else:
             print('Your out with No money..!')  
             break
-
-
-
-
-
-
-
-
-
 
 
 
